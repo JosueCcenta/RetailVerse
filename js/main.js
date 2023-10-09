5e7d8efdb89119edd7ed1bbc7bcdc5cd46350e48
Josue Ccenta <sullon.centa@gmail.com>
2023-10-09 00:04:26 -0500

    El error esta en la linea 98 especificamente en el 2, no me acepta el idboton me sale undefined cuando lo utilizo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 let productos = [];
 
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector(".numero");
+
 fetch("http://localhost:9000/api")
     .then(response => response.json())
     .then(data => {
@@ -7,10 +13,6 @@ fetch("http://localhost:9000/api")
         cargarProductos(productos);
     })
 
-const contenedorProductos = document.querySelector("#contenedor-productos");
-const botonesCategorias = document.querySelectorAll(".boton-categoria");
-const tituloPrincipal = document.querySelector("#titulo-principal");
-let botonesAgregar = document.querySelectorAll(".producto-agregar");
 
 
 function cargarProductos(productosElegidos) {
@@ -61,13 +63,53 @@ function actualizarBotonesAgregar(){
     });
 }
 
-const productosEnCarrito = [];
+let productosEnCarrito;
 
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
 function agregarAlCarrito(e){
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "right", 
+        stopOnFocus: true, 
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem', 
+            y: '1.5rem' 
+          },
+        onClick: function(){} 
+      }).showToast();
+      
     const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === 2);
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
 
-    const productoAgregado = productos.find(producto => producto.id === idBoton)
+    actualizarNumerito();
 
-    console.log(productoAgregado);
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
 
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
 }
